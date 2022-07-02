20c749e183f683fa94b58e016863e950cf1a2db2
Jolene Kearse <70668540+JoleneKearse@users.noreply.github.com>
2022-07-02 19:06:10 -0400

    Upgrade

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-const body = document.getElementsByTagName('body')
-const text = document.getElementById('messageInfo')
+const msg = document.getElementById('textInput').value
+const keyNum = document.getElementById('key').value
 const result = document.getElementById('resultText')
-document.getElementById('encodeBtn').addEventListener('click', encode)
-document.getElementById('decodeBtn').addEventListener('click', decode)
 
-// set up background
-background = new Array(1000).fill('00101')
-body.style.background
+
 // FUNCTIONS FOR CAESAR'S CIPHER
 // check if uppercase
 function isUpperCase(str) {
@@ -14,17 +10,21 @@ function isUpperCase(str) {
 }
 
 // encode each letter
-const encode = (str, key) => {
+function encode(str, key) {
+    console.log(`encode function: ${msg} + ${keyNum}`)
     let encoded = ''
     
-    for (let i=0; i < str.length; i++) {
+    for (let i=0; i < msg.length; i++) {
         // only work on upper or lowercase letters
         if (str[i].match(/[\w]/i)) {
              // if uppercase, return uppercase
             if (isUpperCase(str[i])) {
                 encoded += String.fromCharCode((str.charCodeAt(i) + key - 65) % 26 + 65)
+                console.log(encoded)
             } else {
+                // return lowercase letters
                 encoded += String.fromCharCode((str.charCodeAt(i) + key - 97) % 26 + 97)
+                console.log(encoded)
             }
         } else {
             encoded += str[i]
@@ -33,13 +33,36 @@ const encode = (str, key) => {
     return encoded
 }
 
-const decode = (str, key) => {
+function decode(str, key) {
+    console.log('clicked decode')
+    console.log(`decode function: ${msg} + ${keyNum}`)
     const shift = (26 - key) % 26
     const decoded = encode(str, shift)
     return decoded
 }
 
-const results() {
-    const resultsBox = document.createElement('div')
-    resultsBox.textContent = 
+function showEncodedResult() {
+    result.textContent = encode(msg, keyNum)
 }
+
+function showDecodedResult() {
+    result.textContent = decode(msg, keyNum)
+}
+
+function resetInput() {
+    document.getElementById('myForm').reset()
+    msg.value = ''
+    msg.textContent = ''
+    keyNum.value = 0
+    keyNum.textConent = ''
+    document.location.reload()
+    console.log(`resetBtn: ${msg}`)
+    console.log(`resetBtn: ${keyNum}`)
+}
+
+document.getElementById('encodeBtn').addEventListener('click', showEncodedResult)
+
+document.getElementById('decodeBtn').addEventListener('click', showDecodedResult)
+
+document.getElementById('resetBtn').addEventListener("click", resetInput)
+
